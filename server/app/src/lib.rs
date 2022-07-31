8e89ee2fbf08f4f0bc35c474579a90cbcf0c0475
Ryosuke <korygm33@gmail.com>
2022-07-31 12:21:13 +0900

    refactor: move `db` to a module

--- server/app/src/lib.rs
+++ server/app/src/lib.rs
@@ -1,6 +1,6 @@
 include!("gen/mod.rs");
 
-pub mod data_sources;
 pub mod services;
 
 pub mod config;
+pub mod db;
--- server/app/src/main.rs
+++ server/app/src/main.rs
@@ -3,16 +3,17 @@
 #![allow(clippy::cargo_common_metadata)]
 
 use seichi_game_api::config::{Config, FromEnv, SourceDatabaseConfig};
-use seichi_game_api::data_sources;
 use seichi_game_api::gigantic_minecraft::seichi_game_data::v1::read_service_server::{
     ReadService, ReadServiceServer,
 };
 use seichi_game_api::services::read::ReadServiceImpl;
 use tonic::transport::Server;
 
-async fn initialize_read_service(
+async fn initialize_database_read_service(
     config: &SourceDatabaseConfig,
 ) -> anyhow::Result<impl ReadService> {
+    use seichi_game_api::db::data_sources;
+
     let last_quit_data_source = data_sources::last_quit_data_source(config).await?;
     let break_counts_data_source = data_sources::break_count_data_source(config).await?;
     let build_counts_data_source = data_sources::build_count_data_source(config).await?;
@@ -33,7 +34,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Reading config...");
     let config = Config::from_env()?;
 
-    let service = initialize_read_service(&config.source_database_config)
+    let service = initialize_database_read_service(&config.source_database_config)
         .await
         .expect("Initializing read service");
 
