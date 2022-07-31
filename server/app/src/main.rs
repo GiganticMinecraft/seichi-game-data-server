ff2b58be39c3b59bc7979dbee0221c405b1486f1
Ryosuke <korygm33@gmail.com>
2022-07-31 12:16:28 +0900

    refactor: cleanup initialization code

--- server/app/src/main.rs
+++ server/app/src/main.rs
@@ -10,32 +10,22 @@ use seichi_game_api::gigantic_minecraft::seichi_game_data::v1::read_service_serv
 use seichi_game_api::services::read::ReadServiceImpl;
 use tonic::transport::Server;
 
-async fn initialize_read_service(config: &SourceDatabaseConfig) -> impl ReadService {
-    let expect_message = "Initializing data source";
+async fn initialize_read_service(
+    config: &SourceDatabaseConfig,
+) -> anyhow::Result<impl ReadService> {
+    let last_quit_data_source = data_sources::last_quit_data_source(config).await?;
+    let break_counts_data_source = data_sources::break_count_data_source(config).await?;
+    let build_counts_data_source = data_sources::build_count_data_source(config).await?;
+    let play_ticks_data_source = data_sources::play_ticks_data_source(config).await?;
+    let vote_counts_data_source = data_sources::vote_count_data_source(config).await?;
 
-    let last_quit_data_source = data_sources::last_quit_data_source(config)
-        .await
-        .expect(expect_message);
-    let break_counts_data_source = data_sources::break_count_data_source(config)
-        .await
-        .expect(expect_message);
-    let build_counts_data_source = data_sources::build_count_data_source(config)
-        .await
-        .expect(expect_message);
-    let play_ticks_data_source = data_sources::play_ticks_data_source(config)
-        .await
-        .expect(expect_message);
-    let vote_counts_data_source = data_sources::vote_count_data_source(config)
-        .await
-        .expect(expect_message);
-
-    ReadServiceImpl {
+    Ok(ReadServiceImpl {
         last_quit_data_source,
         break_counts_data_source,
         build_counts_data_source,
         play_ticks_data_source,
         vote_counts_data_source,
-    }
+    })
 }
 
 #[tokio::main]
@@ -43,13 +33,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Reading config...");
     let config = Config::from_env()?;
 
-    let service = initialize_read_service(&config.source_database_config).await;
+    let service = initialize_read_service(&config.source_database_config)
+        .await
+        .expect("Initializing read service");
 
     let serve_address = format!("{}:{}", config.http_config.host, config.http_config.port.0)
         .parse()
-        .unwrap();
+        .expect("Parsing serve address from config");
 
-    println!("Server listening on {}", serve_address);
+    println!("Server will be listening on {}", serve_address);
 
     Server::builder()
         .add_service(ReadServiceServer::new(service))
